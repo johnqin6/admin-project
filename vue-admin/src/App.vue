028ba7d33b07afb1753d7f0562fb7ced7fae4eff
秦振坤 <johnqin@rektec.com.cn>
2019-12-26 10:04:54 +0800

    项目初始化和配置环境变量

---
+++ vue-admin/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app" class="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+  @import url("./assets/css/reset.css");
+  html,
+  body,
+  .app {
+    width: 100%;
+    height: 100%;
+  }
+</style>
